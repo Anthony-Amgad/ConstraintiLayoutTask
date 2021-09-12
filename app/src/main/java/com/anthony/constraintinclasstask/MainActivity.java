0d7cc958345b1dfd648036c11d7bfdbc5da2d378
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2021-09-12 12:56:56 +0200

    Constraint Layout Task Android Booster - Anthony Amgad

---
+++ app/src/main/java/com/anthony/constraintinclasstask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anthony.constraintinclasstask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.red_driverfound);
+    }
+}
